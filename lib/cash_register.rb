41ef52bf5ed93922dbdbef10ec3bd21616c74198
Maria Connie Sanchez <msnchz04@gmail.com>
2020-02-21 00:10:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,12 @@
-
+class CashRegister
+  attr_accessor :cash_register, :cash_register_with_discount 
+  
+  def initialize(total = 0)
+     @total = total 
+  end
+  
+  def discount(cash_register_with_discount)
+    @cash_register_with_discount = 20
+  end 
+  
+end 
